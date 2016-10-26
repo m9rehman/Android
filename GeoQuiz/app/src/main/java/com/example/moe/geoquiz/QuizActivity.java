7bf00292a6415db7ee8ecb95948e37255036fff6
Muhammad Rehman <thatsocliche@hotmail.com>
2016-10-26 16:27:32 -0400

    Wired up next button functionality

--- GeoQuiz/app/src/main/java/com/example/moe/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/moe/geoquiz/QuizActivity.java
@@ -4,13 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    //Creating an array of Question Objects
+    private Question[] mQuestionTextBank = new Question[] {
+            new Question(R.string.question_africa,false),
+            new Question(R.string.question_asia,true),
+            new Question(R.string.question_americas,true),
+            new Question(R.string.question_mideast,false),
+            new Question(R.string.question_oceans,true)
+    };
 
+    private int mCurrentIndex = 0;
+
+    private void updateQuestion(){
+        int question = mQuestionTextBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +37,11 @@ public class QuizActivity extends AppCompatActivity {
         //Getting Reference and Casting
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mQuestionTextView = (TextView) findViewById(R.id.question_textview);
+
+        //Setting the textView by indexing our array of Questions
+        updateQuestion();
 
         //Setting up listeners
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +62,15 @@ public class QuizActivity extends AppCompatActivity {
                         QuizActivity.this,
                         R.string.incorrect_toast,
                         Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Setting up listener for Next button which increments our id
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex++;
+                updateQuestion();
             }
         });
     }
