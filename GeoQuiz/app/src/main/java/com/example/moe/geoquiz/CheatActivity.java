c0a540a3fdc6c22d9b7b177ac1596f24f6691966
Muhammad Rehman <thatsocliche@hotmail.com>
2016-10-30 01:03:15 -0400

    Wired up cheat answer buttons and listeners

--- GeoQuiz/app/src/main/java/com/example/moe/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/example/moe/geoquiz/CheatActivity.java
@@ -4,11 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.example.moe.geoquiz.answer_is_true";
     private boolean mAnswerTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
 
     public static Intent newIntent(Context packageContent,boolean answerIsTrue){
         Intent i = new Intent(packageContent,CheatActivity.class);
@@ -21,6 +26,23 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
-        mAnswerTrue = Bun
+        mAnswerTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
+        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+        mShowAnswer = (Button) findViewById(R.id.showAnswerButton);
+
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mAnswerTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                }   else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
+
+
+
+
     }
 }
