d45d6b7ae6cb4f4c824bd6ee76f7e8e24ead55c4
Muhammad Rehman <thatsocliche@hotmail.com>
2016-11-06 16:27:41 -0500

    Created circular animation

--- GeoQuiz/app/src/main/java/com/example/moe/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/example/moe/geoquiz/CheatActivity.java
@@ -1,10 +1,13 @@
 package com.example.moe.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -45,6 +48,22 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShown(true);
+                
+                int cx = mShowAnswer.getWidth() / 2;
+                int cy = mShowAnswer.getHeight() / 2;
+                float radius = mShowAnswer.getWidth();
+                Animator anim = ViewAnimationUtils
+                        .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                anim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        mAnswerTextView.setVisibility(View.VISIBLE);
+                        mShowAnswer.setVisibility(View.INVISIBLE);
+                    } });
+                anim.start();
+
+
             }
         });
 
